d8a7d20f9f61e147c27107440a93765477d87e19
Brinq q <brinqq>
2024-07-03 08:35:40 -0700

    just some changes

--- scripts/dependencies.ps1
+++ scripts/dependencies.ps1
@@ -5,6 +5,9 @@ winget install --no-upgrade GlazeWM
 winget install --no-upgrade Flow-Launcher.Flow-Launcher
 winget install --no-upgrade Neovim.Neovim
 winget install --no-upgrade wez.wezterm
+winget install --no-upgrade sharkdp.bat
+winget install --no-upgrade starship
 
 echo "[TODO]"
 echo "1) need to add config path to GlazeWM exe args."
+ech0 "2) add |Invoke-Expression(& init powershell)| into powershell profile file"
