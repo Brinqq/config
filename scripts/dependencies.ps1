3ac2fb6309ebbb146d867cc740506b87588785cb
Brinq q <brinqq>
2024-07-03 07:08:22 -0700

    initial commit

---
+++ scripts/dependencies.ps1
@@ -0,0 +1,10 @@
+winget install --no-upgrade eza-community.eza
+winget install --no-upgrade neofetch
+winget install --no-upgrade fzf
+winget install --no-upgrade GlazeWM
+winget install --no-upgrade Flow-Launcher.Flow-Launcher
+winget install --no-upgrade Neovim.Neovim
+winget install --no-upgrade wez.wezterm
+
+echo "[TODO]"
+echo "1) need to add config path to GlazeWM exe args."
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,53 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+local tabSymbols = {"🈲", "🈂️", "🈯", "🈚"};
+
+-- [General]
+config.default_cwd = "C:/main"
+config.default_prog = {"C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"}
+
+-- [Window]
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.98
+config.window_padding = {
+  left = 0, right = 0, top = 15, bottom = 0,
+}
+
+-- [Tabs]
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom =  true
+config.hide_tab_bar_if_only_one_tab = true
+config.colors = { tab_bar = { background = "#282828", active_tab = { bg_color = "#af3a03", fg_color = "#f9f5d7" },}}
+
+-- [Font]
+config.font = wezterm.font("Fira Code", {weight="Medium", stretch="Normal", style="Normal"})
+-- config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
+-- config.font = wezterm.font('Fira Code', { weight = 'Bold', italic = true })
+
+-- [Keybinds]
+config.leader = {key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+  {key = 'j', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain'},
+  { key = 'h', mods = 'LEADER', action = wezterm.action.CloseCurrentTab {confirm  = false}},
+}
+
+for i = 1, 8 do
+  table.insert(config.keys, { key = tostring(i), mods = 'LEADER', action = wezterm.action.ActivateTab(i - 1)});
+end
+
+-- [Events]
+wezterm.on('format-tab-title',
+function(tab, tabs, panes, config, hover, max_width)
+  local title = tabSymbols[tab.tab_index + 1];
+  return title;
+end);
+
+
+-- [Theme]
+config.color_scheme = 'N0Tch2K (Gogh)'
+
+-- [error]
+config.warn_about_missing_glyphs = false
+
+return config
